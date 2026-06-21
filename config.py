241fccbd7eac3951c455c1204a854c4642f99d9a
josearsilva95 <josearsilva95@gmail.com>
2026-06-21 12:05:32 -0300

    fix: corrige indentacao no config.py

--- config.py
+++ config.py
@@ -10,8 +10,8 @@ class Config:
     SUPABASE_KEY = os.getenv("SUPABASE_KEY", "")
     FLASK_ENV = os.getenv("FLASK_ENV", "development")
     SESSION_PERMANENT = False
-EVOLUTION_API_URL = os.getenv("EVOLUTION_API_URL", "")
+    EVOLUTION_API_URL = os.getenv("EVOLUTION_API_URL", "")
     EVOLUTION_INSTANCE = os.getenv("EVOLUTION_INSTANCE", "")
     EVOLUTION_API_KEY = os.getenv("EVOLUTION_API_KEY", "")
     URL_SISTEMA = os.getenv("URL_SISTEMA", "http://localhost:5000")
-    MAX_CONTENT_LENGTH = 16 * 1024 * 1024  # 16MB upload limit
+    MAX_CONTENT_LENGTH = 16 * 1024 * 1024
